772792675484a3ce8d14b1f5781f1daab5a80f4c
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-03-11 00:50:08 +0300

    created two files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+/**
+ * main - entry point
+ * Description: print in standard error
+ * return: Always 1 (failure)
+ */
+int main(void);
+{
+	char *s = "and that piec of art is useful\" - Dora Korpar, 2015-10-19";
+	fwrite(s, sderr);
+	return (1);
+}
