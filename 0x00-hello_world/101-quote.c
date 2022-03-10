35428f00f36be4dea1cdc39ed35f592ac882a955
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-03-11 01:04:59 +0300

    edited

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 /**
  * main - entry point
  * Description: print in standard error
- * return: Always 1 (failure)
+ * Return: Always 1 (failure)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(
+			stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
