771fba1d0c1f6cc887fc597855136ddf53097339
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-03-10 23:48:37 +0300

    created 4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+* main - Entry point
+* Description: Function main prints a string
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
