98be35cdc93618e2eaba51ecbeb8b0e40b1a8c5f
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-03-11 00:03:19 +0300

    created 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - Entry point
+ * Description: the function prints a string
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
