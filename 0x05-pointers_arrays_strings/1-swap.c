cd515c49c76a8ee71e2b5efd0a0d55cdb9853550
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-03-21 15:51:05 +0300

    created 1_swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swap variable values
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+   int temp;
+   temp = *b;
+   *b = *a;
+   *a = temp;
+}
