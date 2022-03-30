e6d02f67c8d46344a6ce51fd628e5d8855af52a4
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-03-30 23:22:54 +0300

    created factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - finds factorial
+ * @n: int
+ * Return: int
+ */
+
+int factorial(int n)
+{
+
+if (n < 0)
+{
+	return (-1);
+}
+else if (n == 0)
+{
+	return (1);
+}
