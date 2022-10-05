c323a5f278b434a4a14beaeee1c7c385ea8dbac5
Yuvenal Njoroge <yuvenalmash@gmail.com>
2022-10-05 15:54:09 +0300

    main

---
+++ 0x1E-search_algorithms/6-main.c
@@ -0,0 +1,11 @@
+int **allocate_map(int n, int m)
+{
+     int **map;
+
+     map = malloc(sizeof(int *) * n);
+     for (size_t i = 0; i < n; i++)
+     {
+          map[i] = malloc(sizeof(int) * m);
+     }
+     return (map);
+}
